e21b6aab3e8610afca4b15e833e47dce2ab17477
Monsieur 486 <monsieur486@gmail.com>
2023-10-24 17:51:50 +0200

    add deposit page

--- src/main/java/com/paymybuddy/paymybuddy/controller/DepositPageController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controller/DepositPageController.java
@@ -1,17 +1,34 @@
 package com.paymybuddy.paymybuddy.controller;
 
+import com.paymybuddy.paymybuddy.dto.form.DepositFormDto;
+import com.paymybuddy.paymybuddy.dto.page.DepositPageDto;
+import com.paymybuddy.paymybuddy.service.page.DepositPageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class DepositPageController {
 
     private static final String ACTIVE_PAGE = "profile";
 
+    private final DepositPageService depositPageService;
+
+    public DepositPageController(DepositPageService depositPageService) {
+        this.depositPageService = depositPageService;
+    }
+
     @GetMapping("/profile/deposit")
-    public String showDepositPage(Model model) {
+    public String showDepositPage(Model model, Principal principal, DepositFormDto depositFormDto) {
         model.addAttribute("activePage", ACTIVE_PAGE);
+
+        DepositPageDto depositPageDto = depositPageService.renderDepositPage(principal.getName(), depositFormDto);
+        model.addAttribute("mail", depositPageDto.getEmail());
+        model.addAttribute("solde", depositPageDto.getSolde());
+        model.addAttribute("depositForm", depositPageDto.getDepositForm());
+
         return "deposit";
     }
 }
--- src/main/java/com/paymybuddy/paymybuddy/service/page/DepositPageService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/page/DepositPageService.java
@@ -1,6 +1,11 @@
 package com.paymybuddy.paymybuddy.service.page;
 
+import com.paymybuddy.paymybuddy.config.ApplicationConfiguration;
+import com.paymybuddy.paymybuddy.dto.form.DepositFormDto;
+import com.paymybuddy.paymybuddy.dto.page.DepositPageDto;
+import com.paymybuddy.paymybuddy.entity.User;
 import com.paymybuddy.paymybuddy.service.UserService;
+import com.paymybuddy.paymybuddy.utils.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +17,29 @@ public class DepositPageService {
 
     public DepositPageService(UserService userService) {
         this.userService = userService;
+    }
+
+    public void depositMoney(String email, Integer amount) {
+        User userDB = userService.findByEmail(email);
+        Integer balance = userDB.getBalance();
+        Integer amountInCents = amount * 100;
+        Double fee = amountInCents * ApplicationConfiguration.DEPOSIT_FEE_PERCENTAGE;
+        Integer newBalance = balance + amount - fee.intValue();
+
+        userService.setBalance(userDB, newBalance);
+    }
+
+    public DepositPageDto renderDepositPage(String principal, DepositFormDto depositFormDto) {
+        DepositPageDto depositPageDto = new DepositPageDto();
+
+        User userDB = userService.findByEmail(principal);
+        depositPageDto.setEmail(userDB.getEmail());
+        depositPageDto.setSolde(StringUtil.getMoney(userDB.getBalance()));
+        if(depositFormDto != null) {
+            DepositFormDto depositFormDto1 = new DepositFormDto();
+        }
+        depositPageDto.setDepositForm(depositFormDto);
+
+        return depositPageDto;
     }
 }
