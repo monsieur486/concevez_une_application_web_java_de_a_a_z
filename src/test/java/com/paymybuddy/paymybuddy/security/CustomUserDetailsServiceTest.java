9e862d26bc8cbff89a50bc1fdd9db84354a89b95
monsieur486 <monsieur486@gmail.com>
2023-10-30 17:52:45 +0100

    add CustomUserDetailsSerice tests

--- src/test/java/com/paymybuddy/paymybuddy/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/paymybuddy/paymybuddy/security/CustomUserDetailsServiceTest.java
@@ -1,21 +1,19 @@
 package com.paymybuddy.paymybuddy.security;
 
 import com.paymybuddy.paymybuddy.entity.User;
-import com.paymybuddy.paymybuddy.repository.MessageRepository;
 import com.paymybuddy.paymybuddy.repository.UserRepository;
-import com.paymybuddy.paymybuddy.service.MessageService;
-import com.paymybuddy.paymybuddy.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceTest {
@@ -29,8 +27,17 @@ class CustomUserDetailsServiceTest {
     @Test
     void loadUserByUsername() {
         User user = new User(1L,"demo@test.fr", "pwd",0);
-        when(dao.findByEmail(any(String.class))).thenReturn(user);
+        when(dao.findByEmail("demo@test.fr")).thenReturn(user);
         UserDetails userDetails = service.loadUserByUsername("demo@test.fr");
         assertEquals("demo@test.fr", userDetails.getUsername());
+    }
+
+    // load User By Invalid Username And Trow UsernameNotFoundException()
+    @Test
+    void loadUserByInvalidUsernameAndTrowUsernameNotFoundException() {
+        when(dao.findByEmail(any())).thenReturn(null);
+        assertThrows(UsernameNotFoundException.class, () -> {
+            service.loadUserByUsername("demo@test.fr");
+        });
     }
 }
