e47a0928aedfe092ec7c34975e6c0a7b5929a43a
Monsieur 486 <monsieur486@gmail.com>
2023-11-13 16:51:38 +0100

    add authentification tests

--- src/test/java/com/paymybuddy/paymybuddy/controller/AuthControllerTest.java
+++ src/test/java/com/paymybuddy/paymybuddy/controller/AuthControllerTest.java
@@ -1,15 +1,19 @@
 package com.paymybuddy.paymybuddy.controller;
 
 import com.paymybuddy.paymybuddy.dto.form.UserFormDto;
+import com.paymybuddy.paymybuddy.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,6 +28,9 @@ class AuthControllerTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @MockBean
+    private UserService userService;
 
     @BeforeEach
     public void setup() {
@@ -47,19 +54,50 @@ class AuthControllerTest {
 
     @Test
     void registration() throws Exception  {
-        UserFormDto user = new UserFormDto();
-        user.setEmail("demo@test.fr");
-        user.setPassword("demo");
-        user.setPasswordForVerification("demo");
+        when(userService.alreadyExist(any(String.class))).thenReturn(false);
 
         this.mockMvc
                 .perform(post("/register/save")
-                        .characterEncoding("UTF-8")
-                        .contentType("application/json")
-                        .content(user.toJson())
+                        .param("email", "demo@test.fr")
+                        .param("password", "Password1!")
+                        .param("passwordForVerification", "Password1!")
                         .with(csrf())
                 )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(model().hasNoErrors())
+        ;
+    }
+
+    @Test
+    void registrationWithInvalidEmail() throws Exception  {
+        when(userService.alreadyExist(any(String.class))).thenReturn(false);
+
+        this.mockMvc
+                .perform(post("/register/save")
+                        .param("email", "demo")
+                        .param("password", "Password1!")
+                        .param("passwordForVerification", "Password1!")
+                        .with(csrf())
+                )
+                .andExpect(model()
+                        .attributeHasFieldErrors("user", "email")
+                )
                 .andExpect(status().isOk())
+        ;
+    }
+
+    @Test
+    void registrationWithExistingEmail() throws Exception  {
+        when(userService.alreadyExist(any(String.class))).thenReturn(true);
+
+        this.mockMvc
+                .perform(post("/register/save")
+                        .param("email", "demo@test.fr")
+                        .param("password", "Password1!")
+                        .param("passwordForVerification", "Password1!")
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
         ;
     }
 }
