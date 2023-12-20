c88012328ddc919d0069c9b76419233e561accb7
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:22:35 +0100

    Improve SpringSecurity class documentation
    Additional comments were added to the SpringSecurity class to improve documentation. The expanded explanations describe the purpose of class methods, constructors, and beans, enhancing readability and understanding for fellow developers.

--- src/main/java/com/paymybuddy/paymybuddy/config/SpringSecurity.java
+++ src/main/java/com/paymybuddy/paymybuddy/config/SpringSecurity.java
@@ -17,6 +17,9 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
+/**
+ * Configuration class for Spring Security.
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
@@ -26,16 +29,34 @@ public class SpringSecurity {
     @Value("${REMEMBER_ME_SECRET}")
     private String rememberMeSecret;
 
+    /**
+     * Constructor for SpringSecurity class.
+     *
+     * @param userDetailsService the service to fetch user details
+     * @param dataSource the data source for the application
+     */
     public SpringSecurity(UserDetailsService userDetailsService, DataSource dataSource) {
         this.userDetailsService = userDetailsService;
         this.dataSource = dataSource;
     }
 
+    /**
+     * Bean for password encoder.
+     *
+     * @return a BCryptPasswordEncoder instance
+     */
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Bean for security filter chain.
+     *
+     * @param http the HttpSecurity instance
+     * @return a SecurityFilterChain instance
+     * @throws Exception if an error occurs
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -72,6 +93,12 @@ public class SpringSecurity {
         return http.build();
     }
 
+    /**
+     * Configures the global authentication manager builder.
+     *
+     * @param auth the AuthenticationManagerBuilder instance
+     * @throws Exception if an error occurs
+     */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
@@ -79,6 +106,11 @@ public class SpringSecurity {
                 .passwordEncoder(passwordEncoder());
     }
 
+    /**
+     * Bean for persistent token repository.
+     *
+     * @return a PersistentTokenRepository instance
+     */
     @Bean
     public PersistentTokenRepository tokenRepository() {
         JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
