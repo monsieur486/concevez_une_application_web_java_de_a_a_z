79cb7f31d4117ce4eecc049ad97a38a70c85f330
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:33:44 +0100

    Add method documentation in LoginPageController
    In this commit, document comments were added for the LoginPageController class and its showAdminPage method. These comments help to clarify the purpose and functionality of the class and method respectively.

--- src/main/java/com/paymybuddy/paymybuddy/controller/LoginPageController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controller/LoginPageController.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * LoginPageController is a Spring MVC Controller that handles requests for the login page.
+ */
 @Controller
 public class LoginPageController {
 
+    /**
+     * This method is mapped to the "/login" URL and is responsible for handling the login requests.
+     * It sets the active page attribute to "login" and returns the name of the view to be rendered.
+     *
+     * @param model the Model object which can be used to add attributes to the view
+     * @return the name of the view to be rendered
+     */
     @RequestMapping(value = "/login")
     public String showAdminPage(Model model) {
         String activePage = "login";
