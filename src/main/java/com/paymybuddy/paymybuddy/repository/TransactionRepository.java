a3d65f087fd2f067d9f2bd292ccc80c6aa254d05
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:19:15 +0100

    Add comments to UserRepository and TransactionRepository interfaces
    The UserRepository and TransactionRepository interfaces have been updated to include comments that describe their methods and purpose. These comments not only serve as documentation, but also enhance code readability and understanding for other developers working on the project.

--- src/main/java/com/paymybuddy/paymybuddy/repository/TransactionRepository.java
+++ src/main/java/com/paymybuddy/paymybuddy/repository/TransactionRepository.java
@@ -7,14 +7,37 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for Transaction entity.
+ * This interface extends CrudRepository to provide CRUD functionality for the Transaction entity.
+ */
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 
+    /**
+     * Find a transaction by its id.
+     * @param id The id of the transaction.
+     * @return The transaction with the given id.
+     */
     Transaction findById(long id);
 
+    /**
+     * Find all transactions associated with a given user, ordered by id in descending order.
+     * @param user The user whose transactions are to be found.
+     * @param pageable The pagination information.
+     * @return A page of transactions associated with the given user.
+     */
     Page<Transaction> findAllByConnection_UserOrderByIdDesc(User user, Pageable pageable);
 
+    /**
+     * Delete a transaction by its connection id.
+     * @param id The connection id of the transaction to be deleted.
+     */
     void deleteByConnectionId(Long id);
 
+    /**
+     * Delete transactions by their connection id.
+     * @param id The connection id of the transactions to be deleted.
+     */
     void deleteTransactionsByConnection_Id(Long id);
 }
--- src/main/java/com/paymybuddy/paymybuddy/repository/UserRepository.java
+++ src/main/java/com/paymybuddy/paymybuddy/repository/UserRepository.java
@@ -1,13 +1,30 @@
 package com.paymybuddy.paymybuddy.repository;
 
-
 import com.paymybuddy.paymybuddy.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * UserRepository is an interface that extends CrudRepository.
+ * It provides methods to perform CRUD operations on User entities.
+ * It also provides additional methods to find a User by email and check if a User exists by email.
+ */
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    /**
+     * Finds a User entity by its email.
+     *
+     * @param email the email of the User entity to be found.
+     * @return the User entity with the given email or null if no such User entity exists.
+     */
     User findByEmail(String email);
 
+    /**
+     * Checks if a User entity exists by its email.
+     *
+     * @param email the email of the User entity to be checked.
+     * @return true if a User entity with the given email exists, false otherwise.
+     */
     Boolean existsByEmail(String email);
 }
