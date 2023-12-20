b3e0e46b9c8081791fbb4c9721f9b43c0c10db0b
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:01:22 +0100

    Correct the commit message to:
    Add explanatory comments to MessageService methods
    
    Added detailed explanations in comment form to MessageService methods for better understanding of the code structure. These comments enhance code readability which ultimately aids in system maintenance.

--- src/main/java/com/paymybuddy/paymybuddy/service/MessageService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/MessageService.java
@@ -6,16 +6,29 @@ import com.paymybuddy.paymybuddy.repository.MessageRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling message related operations.
+ */
 @Service
 @Slf4j
 public class MessageService {
 
     private final MessageRepository messageRepository;
 
+    /**
+     * Constructor for MessageService.
+     *
+     * @param messageRepository  the message repository
+     */
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
+    /**
+     * Saves a new message to the repository.
+     *
+     * @param contactFormDto  the contact form data transfer object
+     */
     public void saveMessage(ContactFormDto contactFormDto) {
         Message message = new Message();
         message.setEmail(contactFormDto.getEmail());
