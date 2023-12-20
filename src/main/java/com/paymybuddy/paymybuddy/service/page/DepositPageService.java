81612cdcfb893e8051ab52c7c1f0e24157e55e39
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:10:02 +0100

    Add comments to ConnectionService methods
    Added explanatory comments to the ConnectionService methods. The comments provide a clear understanding of what each method is doing, improving code readability and making it easier for future maintenance or updates.

--- src/main/java/com/paymybuddy/paymybuddy/service/page/DepositPageService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/page/DepositPageService.java
@@ -11,19 +11,35 @@ import com.paymybuddy.paymybuddy.tools.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling operations related to the deposit page.
+ */
 @Service
 @Slf4j
 public class DepositPageService {
 
     private final UserService userService;
-
     private final Bank bank;
 
+    /**
+     * Constructor for the DepositPageService.
+     *
+     * @param userService the user service
+     * @param bank the bank service
+     */
     public DepositPageService(UserService userService, Bank bank) {
         this.userService = userService;
         this.bank = bank;
     }
 
+    /**
+     * Method to deposit money into a user's account.
+     *
+     * @param email the email of the user
+     * @param amount the amount to be deposited
+     * @param depositInformation the deposit information
+     * @return true if the deposit was successful, false otherwise
+     */
     public Boolean depositMoney(String email, Integer amount, DepositInformation depositInformation) {
         User userDB = userService.findByEmail(email);
         Integer balance = userDB.getBalance();
@@ -42,6 +58,13 @@ public class DepositPageService {
         return true;
     }
 
+    /**
+     * Method to render the deposit page.
+     *
+     * @param principal the principal user
+     * @param depositForm the deposit form
+     * @return the deposit page DTO
+     */
     public DepositPageDto renderDepositPage(String principal, DepositFormDto depositForm) {
         DepositPageDto depositPageDto = new DepositPageDto();
 
