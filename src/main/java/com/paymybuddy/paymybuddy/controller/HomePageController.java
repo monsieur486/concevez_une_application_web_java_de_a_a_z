7cf252835c0cb8b8d45fd3795affc667128d0290
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:29:44 +0100

    Add Javadoc comments to HomePageController
    Detailed Javadoc comments have been added to the HomePageController class. This provides a better understanding of the class functions and methods and enhances code readability, which is beneficial for further development and maintenance.

--- src/main/java/com/paymybuddy/paymybuddy/controller/HomePageController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controller/HomePageController.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * This is a controller class for handling requests related to the home page.
+ */
 @Controller
 public class HomePageController {
 
+    /**
+     * This method is mapped to the root ("/") URL and is responsible for showing the home page.
+     * It adds an attribute "activePage" to the model with a value of "home".
+     *
+     * @param model the Model object to which attributes for the view can be added
+     * @return a string that represents the name of the view (in this case, "index")
+     */
     @GetMapping("/")
     public String showHomePage(Model model) {
         String activePage = "home";
