f307ce186544f0f1dbd3b67023c4d98303d3517d
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:17:25 +0100

    Add comments to MessageRepository interface
    The message repository interface has been updated to include explanatory comments. These comments describe the purpose of MessageRepository and its role in performing CRUD operations on message entities, improving code readability for other developers.

--- src/main/java/com/paymybuddy/paymybuddy/repository/MessageRepository.java
+++ src/main/java/com/paymybuddy/paymybuddy/repository/MessageRepository.java
@@ -3,6 +3,10 @@ package com.paymybuddy.paymybuddy.repository;
 import com.paymybuddy.paymybuddy.entity.Message;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * MessageRepository is an interface that extends CrudRepository.
+ * It provides methods to perform CRUD operations on Message entities.
+ */
 public interface MessageRepository extends CrudRepository<Message, Long> {
 
 }
