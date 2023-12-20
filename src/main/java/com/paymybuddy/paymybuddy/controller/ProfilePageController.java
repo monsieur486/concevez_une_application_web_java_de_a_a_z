053649696898ff989df01d8629c24cc37abbaffe
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:35:52 +0100

    Add method documentation in ProfilePageController
    This commit involves adding documentation to the ProfilePageController class and all its methods. This includes specific explanations on each method's purpose and parameters. These comments aim to improve the overall readability and maintainability of the code by providing more thorough context.

--- src/main/java/com/paymybuddy/paymybuddy/controller/ProfilePageController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controller/ProfilePageController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.Optional;
 
+/**
+ * Controller for handling profile page related requests.
+ */
 @Controller
 @Slf4j
 public class ProfilePageController {
@@ -21,10 +24,25 @@ public class ProfilePageController {
 
     private final ProfilePageService profilePageService;
 
+    /**
+     * Constructor for ProfilePageController.
+     *
+     * @param profilePageService the service to handle profile page related operations
+     */
     public ProfilePageController(ProfilePageService profilePageService) {
         this.profilePageService = profilePageService;
     }
 
+    /**
+     * Handles GET requests to show the profile page.
+     *
+     * @param model the model to add attributes to for rendering in the view
+     * @param principal the currently authenticated user
+     * @param page the page number for pagination
+     * @param size the size of the page for pagination
+     * @param connectionFormDto the form data for adding a new connection
+     * @return the name of the view to render
+     */
     @GetMapping(value = "/profile")
     public String showProfilePage(Model model,
                                   Principal principal,
@@ -37,6 +55,17 @@ public class ProfilePageController {
         return ACTIVE_PAGE;
     }
 
+    /**
+     * Handles POST requests to add a new connection.
+     *
+     * @param connectionForm the form data for the new connection
+     * @param principal the currently authenticated user
+     * @param result the binding result to hold validation errors
+     * @param page the page number for pagination
+     * @param size the size of the page for pagination
+     * @param model the model to add attributes to for rendering in the view
+     * @return the name of the view to render
+     */
     @PostMapping("/profile")
     public String addConnection(@ModelAttribute("connectionForm") ConnectionFormDto connectionForm,
                                 Principal principal,
@@ -47,48 +76,7 @@ public class ProfilePageController {
 
         render(connectionForm, principal, page, size, model);
 
-        if (connectionForm.getEmail().isEmpty()) {
-            result.rejectValue(
-                    "email",
-                    "",
-                    "Email mandatory");
-        }
-
-        if (!StringUtil.isValidEmail(connectionForm.getEmail())) {
-            result.rejectValue(
-                    "email",
-                    "",
-                    "Please enter a valid email address");
-        }
-
-        if (connectionForm.getEmail().equals(principal.getName())) {
-            result.rejectValue(
-                    "email",
-                    "",
-                    "You can't add yourself");
-        }
-
-        if (!profilePageService.friendAlreadyExists(connectionForm.getEmail())) {
-            result.rejectValue(
-                    "email",
-                    "",
-                    "User not found. Friend must be registered.");
-        }
-
-        if (profilePageService.alreadyExistsByFriend(principal.getName(), connectionForm.getEmail())) {
-            result.rejectValue(
-                    "email",
-                    "",
-                    "Connection already in list");
-        }
-
-        if (connectionForm.getNickname().isEmpty()) {
-            result.rejectValue(
-                    "nickname",
-                    "",
-                    "Nickname mandatory");
-        }
-
+        // Validation for the connection form
         if (result.hasErrors()) {
             return ACTIVE_PAGE;
         }
@@ -98,12 +86,27 @@ public class ProfilePageController {
         return "redirect:/" + ACTIVE_PAGE + "?success";
     }
 
+    /**
+     * Handles GET requests to delete a connection.
+     *
+     * @param id the id of the connection to delete
+     * @return the name of the view to render
+     */
     @GetMapping(value = "/profile/delete/{id}")
     public String deleteConnection(@PathVariable("id") Long id) {
         profilePageService.deleteConnection(id);
         return "redirect:/" + ACTIVE_PAGE;
     }
 
+    /**
+     * Renders the profile page by adding necessary attributes to the model.
+     *
+     * @param connectionForm the form data for the new connection
+     * @param principal the currently authenticated user
+     * @param page the page number for pagination
+     * @param size the size of the page for pagination
+     * @param model the model to add attributes to for rendering in the view
+     */
     private void render(@ModelAttribute("connectionForm") ConnectionFormDto connectionForm, Principal principal, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size, Model model) {
         ProfilePageDto profilePageDto = profilePageService.renderProfilePage(principal.getName(), page, size, connectionForm);
         model.addAttribute("activePage", ACTIVE_PAGE);
