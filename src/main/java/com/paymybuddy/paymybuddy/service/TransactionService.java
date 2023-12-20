5975ef9261284c3f8226ec1232e4aa5eae8825e2
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 22:00:02 +0100

    Add explanatory comments to TransactionService methods
    Added detailed explanations in comment form to TransactionService methods for better understanding of the code structure. These comments enhance code readability which ultimately aids in system maintenance.

--- src/main/java/com/paymybuddy/paymybuddy/service/TransactionService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/TransactionService.java
@@ -10,6 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling transaction related operations.
+ */
 @Service
 @Slf4j
 @Transactional
@@ -17,10 +20,22 @@ public class TransactionService {
 
     private final TransactionRepository transactionRepository;
 
+    /**
+     * Constructor for TransactionService.
+     *
+     * @param transactionRepository  the transaction repository
+     */
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
 
+    /**
+     * Adds a new transaction to the repository.
+     *
+     * @param connection   the connection object
+     * @param amount       the amount of the transaction
+     * @param description  the description of the transaction
+     */
     public void addTransaction(Connection connection, Integer amount, String description) {
         Transaction transaction = new Transaction();
         transaction.setConnection(connection);
@@ -30,10 +45,23 @@ public class TransactionService {
         transactionRepository.save(transaction);
     }
 
+    /**
+     * Retrieves a page of transactions for a user.
+     *
+     * @param user  the user object
+     * @param page  the page number
+     * @param size  the page size
+     * @return      a page of transactions
+     */
     public Page<Transaction> getTransactions(User user, int page, int size) {
         return transactionRepository.findAllByConnection_UserOrderByIdDesc(user, PageRequest.of(page, size));
     }
 
+    /**
+     * Deletes transactions by connection id.
+     *
+     * @param id  the id of the connection
+     */
     public void deleteTransactionsByConnectionId(Long id) {
         transactionRepository.deleteTransactionsByConnection_Id(id);
     }
