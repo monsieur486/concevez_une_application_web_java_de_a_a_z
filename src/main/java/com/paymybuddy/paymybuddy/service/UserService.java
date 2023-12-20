d734baf1784d79c4cfa145e28ef86cbbe941f7d9
Monsieur 486 <monsieur486@gmail.com>
2023-12-20 21:57:31 +0100

    Add comprehensive comments to UserService methods
    Added explanatory comments to all methods in the UserService class to make the code clearer and easier to maintain. This will facilitate understanding of the functions and overall structure of the UserService class.

--- src/main/java/com/paymybuddy/paymybuddy/service/UserService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/UserService.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling user related operations.
+ */
 @Service
 @Slf4j
 public class UserService {
@@ -14,12 +17,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructor for UserService.
+     *
+     * @param userRepository   the user repository
+     * @param passwordEncoder  the password encoder
+     */
     public UserService(UserRepository userRepository,
                        PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Saves a new user to the repository.
+     *
+     * @param userFormDto  the user form data transfer object
+     */
     public void saveUser(UserFormDto userFormDto) {
         User user = new User();
         user.setEmail(userFormDto.getEmail());
@@ -32,17 +46,35 @@ public class UserService {
         log.info("User created : {}", userFormDto.getEmail());
     }
 
+    /**
+     * Finds a user by email.
+     *
+     * @param email  the email of the user
+     * @return       the user object
+     */
     public User findByEmail(String email) {
         log.debug("Find user by email : {}", email);
         return userRepository.findByEmail(email);
     }
 
+    /**
+     * Sets the balance for a user.
+     *
+     * @param user     the user object
+     * @param balance  the balance to be set
+     */
     public void setBalance(User user, Integer balance) {
         user.setBalance(balance);
         userRepository.save(user);
         log.info("User {} balance updated : {}", user, balance);
     }
 
+    /**
+     * Checks if a user already exists by email.
+     *
+     * @param email  the email of the user
+     * @return       true if the user exists, false otherwise
+     */
     public Boolean alreadyExist(String email) {
         log.debug("Check if user already exist : {}", email);
         return userRepository.existsByEmail(email);
